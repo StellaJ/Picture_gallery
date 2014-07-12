e660b70b6b90c6653467421f561891e7fc352821
Stella <stella@example.com>
2014-07-12 18:24:05 +0200

    add bootstrap and layout

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,6 +1,6 @@
 class CategoriesController < ApplicationController
 
-  layout false
+  layout 'admin'
 
   def index
     @categories = Category.sort
@@ -14,6 +14,7 @@ class CategoriesController < ApplicationController
   def create
     @categories = Category.new(category_params)
     if @categories.save
+      flash[:notice] = "Category was successfully created"
       redirect_to(:action=>'index')
     else
       @counter = Category.count + 1
@@ -33,6 +34,7 @@ class CategoriesController < ApplicationController
   def update
     @category = Category.find(params[:id])
     if @category.update_attributes(category_params)
+       flash[:notice] = "Category was successfully updated"
       redirect_to(:action=>'show', :id => @category.id)
     else
       @counter = Category.count
@@ -46,6 +48,7 @@ class CategoriesController < ApplicationController
 
   def sakujo_suru
     category = Category.find(params[:id]).destroy
+     flash[:notice] = "Category was successfully deleted"
     redirect_to(:action=>'index')
   end
 
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+	def status_od_visibility(boolean, options={})
+		  options[:true_text] ||= ' '
+		  options[:false_text] ||= ' '
+		if boolean
+			content_tag(:span, options[:true_text], :class => 'glyphicon glyphicon-ok')
+		else
+			content_tag(:span, options[:true_text], :class => 'glyphicon glyphicon-remove')
+		end
+	end
 end
